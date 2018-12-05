7de3e7a4309e0ab25b6c3f32a872f5f1a523daa9
pistachio05 <malaya@uci.edu>
2018-12-04 18:24:09 -0800

    Create engage.py

---
+++ engage.py
@@ -0,0 +1,58 @@
+import json, re
+from nltk.stem.lancaster  import LancasterStemmer 
+
+def stemList(listOfTokens):  
+    Porter = LancasterStemmer()
+    for i in range(len(listOfTokens)):
+        listOfTokens[i] = Porter.stem(listOfTokens[i])
+
+#queryList = ['informatics', 'mondego', 'irvine', 'artificial', 'computer']
+
+def init():
+    # get user input and split it on white space and stemm it
+    queryList = input("Enter Search word:").strip().split()
+    stemList(queryList)
+    print(queryList)
+    result = []
+    #load the index as dict
+    data = json.load(open("./output/hoang.json"))
+
+    for query in queryList:
+        try:
+            # get the posting list for the query if in index
+            postingList = data[query.lower()]
+        except:
+            #query not in index, who care move on!
+            print("WE DONT HAVE IT!")
+            continue
+        # list of Url sets (for every query) 
+        result.append(setOfURLS(postingList))
+    
+    print(result)    
+    x = result[0].intersection(*result[1:])
+    for x in x:
+        print(x)
+        with open('r.txt','a') as w:
+            w.write(x)   
+            w.write("\n") 
+
+
+
+def setOfURLS(postingList):
+    rset = set()
+    #load bookkeeping file as dict
+    bookkeepingJson = json.load(open('./WEBPAGES_RAW/bookkeeping.json'))
+    # i is list of ti-dfi and docId
+    for i in postingList:
+            # match bookkeeping ID
+            print(i)
+            docDirNum, docFileNum = divmod(int(i[0]), 1000)
+            docID = str(docDirNum) + "/" + str(docFileNum)
+
+            # once we have docID it alawys exists in bookkeeping dict
+            targetURL = bookkeepingJson[docID]
+            rset.add(targetURL)
+            #print("DocID: %s URL: %s"  %(str(docID),targetURL))
+    return rset
+
+init()
